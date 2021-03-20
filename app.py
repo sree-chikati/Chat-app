c5e9bb92198ee7a626a9fe333a81258401158a0e
sree-chikati <srechi453.fusdk12@gmail.com>
2021-03-20 13:16:14 -0700

    [edit] app.py with routes

--- app.py
+++ app.py
@@ -1,4 +1,17 @@
-from app import app
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_socketio import SocketIO, join_room, leave_room, emit
+from flask_session import Session
+
+app = Flask(__name__)
+
+##########################################
+#              Main Routes               #
+##########################################
+
+@app.route('/')
+def index():
+    '''Display homepage'''
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
