4e66f293f146469b7c5081ef136c210a5b32839c
sree-chikati <srechi453.fusdk12@gmail.com>
2021-03-21 17:18:06 -0700

    [add] route 'join'

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ app.config['SESSION_TYPE'] = 'filesystem'
 
 Session(app)
 
+socketio = SocketIO(app, manage_session=False)
+
 ##########################################
 #              Main Routes               #
 ##########################################
@@ -31,6 +33,13 @@ def chat():
             return render_template('chat.html', session = session)
         else:
             return redirect(url_for('index'))
+
+@socketio.on('join', namespace='/chat')
+def join(message):
+    room = session.get('room')
+    join_room(room)
+    emit('status', {'msg':  session.get('username') + ' has entered the room.'}, room=room)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
